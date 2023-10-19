c7b4cbb9a039f6d829e5cc224fbc5191e4bfdf23
Dayton <daytonvanwilsem@gmail.com>
2023-10-19 10:40:14 +0200

    fullstack

--- admin.php
+++ admin.php
@@ -8,16 +8,48 @@ $database = "fullstack";
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Fullstack</title>
+    <title>fullstack</title>
 </head>
 <body>
-    <h1>Bands</h1>
-    <form action="process_form.php" method="POST">
+    <h1>Band Informatie</h1>
+    <form action="process_form.php?type=0" method="POST">
         <label for="band_naam">Band Naam:</label>
         <input type="text" name="band_naam" required><br><br>
         <label for="genre">Genre:</label>
-        <input type="text" name="genre" required><br><br>
+        <select name="genre" required>
+            <option value="Rock">Rock</option>
+            <option value="Pop">Pop</option>
+            <option value="Hip-Hop">Hip-Hop</option>
+            <option value="Electronic">Electronic</option>
+            <!-- Add more genre options here -->
+        </select><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+<head>
+    <title>Event Form</title>
+</head>
+<body>
+    <h1>Event Form</h1>
+    <form method="post" action="process_form.php?type=1">
+        <label for="datum">Datum:</label>
+        <input type="date" name="datum" required><br><br>
+
+        <label for="aanvangstijd">Aanvangstijd:</label>
+        <input type="time" name="aanvangstijd" required><br><br>
+
+        <label for="naam_event">Naam event:</label>
+        <input type="text" name="naam_event" required><br><br>
+
+        <label for="entreeprijs">Entreeprijs:</label>
+        <input type="number" step="0.01" name="entreeprijs" required><br><br>
+
         <input type="submit" value="Submit">
     </form>
 </body>
 </html>
+
+
+
+
--- process_form.php
+++ process_form.php
@@ -6,20 +6,40 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
-// Retrieve form data
-$band_naam = $_POST["band_naam"];
-$genre = $_POST["genre"];
+$connection = $mysqli; // Initialize $connection with $mysqli
 
-// Prepare and execute the SQL insert statement
-$query = "INSERT INTO band (band_naam, genre) VALUES (?, ?)";
-if ($stmt = $mysqli->prepare($query)) {
-    $stmt->bind_param("ss", $band_naam, $genre);
-    if ($stmt->execute()) {
-        echo "Band information has been successfully added to the database.";
-    } else {
-        echo "Error: " . $stmt->error;
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_GET['type'] == 0) {
+    // Retrieve form data
+    $band_naam = $_POST["band_naam"];
+    $genre = $_POST["genre"];
+
+    // Prepare and execute the SQL insert statement
+    $query = "INSERT INTO band (band_naam, genre) VALUES (?, ?)";
+    if ($stmt = $mysqli->prepare($query)) {
+        $stmt->bind_param("ss", $band_naam, $genre);
+        if ($stmt->execute()) {
+            echo "Band information has been successfully added to the database.";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        $stmt->close();
     }
-    $stmt->close();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_GET['type'] == 1) {
+    $datum = $_POST["datum"];
+    $aanvangstijd = $_POST["aanvangstijd"];
+    $naam_event = $_POST["naam_event"];
+    $entreeprijs = $_POST["entreeprijs"];
+
+    // Insert data into the "event" table
+    $sql = "INSERT INTO event (datum, aanvangstijd, naam_event, entreeprijs) VALUES ('$datum', '$aanvangstijd', '$naam_event', $entreeprijs)";
+
+    if ($mysqli->query($sql)) { // Use $mysqli to execute the query
+        echo "Event data inserted successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $mysqli->error;
+    }
 }
 
 // Close the database connection
